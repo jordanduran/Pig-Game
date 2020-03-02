abd3e993e57acf56a7aeae9022a7ed427e6e5f00
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-03-01 19:58:26 -0500

    Manipulating the DOM to change dice imgs

--- app.js
+++ app.js
@@ -15,15 +15,28 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 1;
 
-// Calculate a random number
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
-
 document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML =
 //   '<em>' + dice + '</em>';
-
-let x = document.querySelector('#score-0').textContent;
-console.log(x);
+// let x = document.querySelector('#score-0').textContent;
+// console.log(x);
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // Calculate a random number
+  let dice = Math.floor(Math.random() * 6) + 1;
+
+  // Display the results
+  let diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = './images/dice-' + dice + '.png';
+
+  // Update the round score IF the rolled number was NOT a 1
+});
+1;
