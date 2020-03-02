ffdb20255db61a795682b5e270b74d0052ab42d5
Jordan Duran <jordanduran@Jordans-MacBook-Pro.local>
2020-03-01 21:16:47 -0500

    Implementing the Hold function

--- app.js
+++ app.js
@@ -23,32 +23,53 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   diceDOM.src = './images/dice-' + dice + '.png';
 
   // Update the round score IF the rolled number was NOT a 1
-
   if (dice !== 1) {
     // Add score
-
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
     // Next player
-
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-
-    // Set round score back to 0
-
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '1';
-
-    // Toggle active class --add/remove--
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    // Remove dice when player rolls a 1
-
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer();
   }
 });
-1;
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+  // Set round score back to 0
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  // Toggle active class --add/remove--
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  // Remove dice when player rolls a 1
+  document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // Add current score to GLOBAL score
+  scores[activePlayer] += roundScore;
+
+  // Update the UI
+  document.querySelector('#score-' + activePlayer).textContent =
+    scores[activePlayer];
+
+  // Check if player won the game
+  if (scores[activePlayer] >= 100) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+    document
+      .querySelector('.player-' + activePlayer + '-panel')
+      .classList.add('winner');
+    document
+      .querySelector('.player-' + activePlayer + '-panel')
+      .classList.remove('active');
+  } else {
+    // Next player
+    nextPlayer();
+  }
+});
